0e5f59b49cd90ddd64f3bfee9dfa1783496d697c
Lenin0 <pedrolenin06@gmail.com>
2022-05-04 09:28:54 -0300

    Validação de campos de formulário

---
+++ php/validação/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport"  content="width=device-width, inital-scale=l">
+</head>
+
+<body>
+    <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</P>
+
+    <form action="script.php" method="post">
+        <p>Seu nome: <input type= "text" name="nome" /></p>
+        <p>Sua idade: <input type= "text" name="idade" /></p>
+        <p><input type="submit" value="Enviar dados do competidor"/></p>
+    </form>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
---
+++ php/validação/script.php
@@ -0,0 +1,57 @@
+<?php
+
+$categorias=[];
+$categorias[]= 'infantil';
+$categorias[]= 'adolecentes';
+$categorias[]= 'adulto';
+
+$nome = $_POST['nome'];
+$idade= $_POST['idade'];
+
+if(empty($nome))
+{
+    echo 'O nome não pode ser vazio';
+    return;
+}
+if(strlen($nome) < 3)
+{
+    echo 'O nome deve conter mais de 3 caracteres';
+    return;
+}
+
+if(strlen($nome) > 40)
+{
+    echo 'O nome é muito extenso';
+    return;
+}
+
+if(!is_numeric($idade))
+{
+    echo 'Informe um número para idade';
+    return;
+}
+
+if($idade >= 6 && $idade == 12)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo " o nadador " .$nome. " compete na categoria " .$categorias[$i];
+    }
+}
+else if($idade >= 13 && $idade <= 18)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolecente')
+            echo " o nadador " .$nome. " compete na categoria " .$categorias[$i];
+    }
+}
+else
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+            echo " o nadador " .$nome. " compete na categoria " .$categorias[$i];
+    }
+} 
